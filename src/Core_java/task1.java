aa13090e7167381d23b34344722d33a76c18caed
devarsh.shukla <devarshshukla64@gmail.com>
2025-05-14 16:16:49 +0530

    Initial commit

---
+++ src/Core_java/task1.java
@@ -0,0 +1,18 @@
+package Core_java;
+
+public class task1 {
+    public static void main(String[] args) {
+        String name = "dee";
+        int age = 99;
+        float height = 6.3f;
+        boolean isstudent = false;
+        System.out.println("name is " + name);
+        System.out.println("age is  " + age);
+        System.out.println("height is "  + height);
+        System.out.println("is student is " + isstudent);
+
+
+
+
+    }
+}
