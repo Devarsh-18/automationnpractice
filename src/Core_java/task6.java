fd210870035b386008d8a4e046ceb5929c1e98a4
devarsh.shukla <devarshshukla64@gmail.com>
2025-05-14 18:30:52 +0530

    Initial commit

---
+++ src/Core_java/task6.java
@@ -0,0 +1,17 @@
+package Core_java;
+
+public class task6 {
+    public static void main(String[] args) {
+        int i = 42;
+        double d = 3.14;
+        String c = "j";
+        boolean b = true;
+        String cc = "java practice";
+        System.out.println("Integer = " + i);
+        System.out.println("character = " + c);
+        System.out.println("Floating-point: " + d);
+        System.out.println("boolean " + b);
+        System.out.println("string " + cc);
+
+
+    }}
